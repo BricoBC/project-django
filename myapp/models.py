555c6bdfc915c03df5201af86646a386f9d57e6d
BricoBC <barajas.c.bruno.n@gmail.com>
2023-10-25 14:09:09 -0600

    Modify: Add new class to a model and migrate it.

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Las clases son las tablas
 class Project(models.Model):
     name = models.CharField(max_length=255)
+    #Los atributos con las columnas
+#Tabla: Project, con la columna id y name.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
